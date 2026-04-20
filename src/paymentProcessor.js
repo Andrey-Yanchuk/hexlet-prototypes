70185051c5bc62c9557f06fc29a8e6f18b043c97
Yanchuk Andrey <orpiy@mail.ru>
2026-04-20 23:36:48 +0700

    feat: add logging decorator and usage examples

--- src/paymentProcessor.js
+++ src/paymentProcessor.js
@@ -1,7 +1,10 @@
+// src/paymentProcessor
+// Задание по теме: Иерархия типов
 import { CreditCardPayment } from "./CreditCardPayment.js";
 import { PayPalMethod } from "./PayPalMethod.js";
 import { CryptoPayment } from "./CryptoPayment.js";
-
+import { PaymentWithLogging } from "./PaymentWithLogging.js";
+/*-----------------------------------------------------*/
 const processPayments = (payments, amount) => {
     payments.forEach((payment) => {
         payment.pay(amount);
@@ -14,3 +17,11 @@ const payments = [
     new CryptoPayment("0xABC123"),
 ]
 processPayments(payments, 100);
+/*-----------------------------------------------------*/
+// Пример, где наследование плохая идея, потому что retry это поведение, а не вид оплат и это не отношение "частное-общее"
+// class RetryPayment extends PayPalPayment { ... }
+/*-----------------------------------------------------*/
+// Пример, где лучше композиция вместо наследования
+// Почему композиция лучше здесь, добавляет поведение без изменения иерархии, можно комбинировать: логирование, retry, кэширование, не ломает контракт базового типа
+const payment = new PaymentWithLogging(new CreditCardPayment("1234", "123"));
+payment.pay(100);
